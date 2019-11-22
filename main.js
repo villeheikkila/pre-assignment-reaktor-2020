ea83178d1f760866faff17d746619c78808ad364
Ville Heikkilä <tehomail@gmail.com>
2019-11-22 15:30:25 +0200

    Remove the dependency version requirements

--- main.js
+++ main.js
@@ -2,29 +2,35 @@ document.addEventListener("DOMContentLoaded", () => {
 	document.getElementById('fetchData').addEventListener('click', async (event) => {
 		event.preventDefault();
 
-		// Fetch the local file
+		// Fetch the local status.real file
 		const getData = await fetch('status.real', { mode: 'no-cors' })
 		const textData = await getData.text()
 
-		// Packages are split with a blank line
+		// Split the text file by package
 		const arrayData = textData.split("\n\n")
 
 		// Format the data to a nice array of objects
 		const data = arrayData.reduce((collected, element) => {
-			const splitElement = element.split("\n")
-			const filter = splitElement.filter(line => line.startsWith("Package") || line.startsWith("Depends"))
+			// Remove all unneeded lines
+			const filter = element.split("\n").filter(line => line.startsWith("Package") || line.startsWith("Depends"))
+
+			// Parse the relevant package and dependency information
 			const package = filter[0] && filter[0].replace("Package: ", "")
-			const dependencies = filter[1] ? filter[1].replace("Depends: ", "").split(", ") : null;
+			const depWithVersions = filter[1] ? filter[1].replace("Depends: ", "").split(", ") : [];
+			const dependencies = depWithVersions.map(dep => dep.length > 1 ? dep.split(" ")[0] : dep)
+
 			const result = {
 				package,
 				dependencies,
 			}
+
 			collected.push(result)
 			return collected;
 
 		}, [])
 
-		console.log(data)
+		console.log("depTrimmed", data)
+
 
 	})
 });
